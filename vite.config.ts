824a315c9e573072b3d6c6bf2370e32eb399e834
Rawan Amr <rawanamr357@gmail.com>
2025-08-19 17:47:33 +0300

    fix gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/products_builder",
+  base: "/products_builder/",
   plugins: [react(), tailwindcss()],
 });
