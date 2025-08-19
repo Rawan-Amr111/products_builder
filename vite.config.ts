1d383802c31943601b4c28d7ff5df1a1d73e287b
Rawan Amr <rawanamr357@gmail.com>
2025-08-19 18:00:14 +0300

    fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/products_builder/",
   plugins: [react(), tailwindcss()],
 });
